7c1c4c40941473cb9da238b704e176ba56d4ef43
fdeantoni <fdeantoni@gmail.com>
2025-09-05 09:59:04 +0200

    Update build to extract version number from cargo metadata.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+shopt -s nocasematch
+
+get_version() {
+  local version=`cargo metadata --format-version 1 | jq -r '.packages[] | select( .name == "echo-server" ) | .version ' | tr '[:upper:]' '[:lower:]'`
+  echo $version
+}
 
 EXTRA_OPTS=$1
 TAG="fdeantoni/echo-server"
 
-docker buildx build ${EXTRA_OPTS} --platform linux/amd64,linux/arm64/v8 -t ${TAG}:0.4.0 -t ${TAG}:latest -f docker/Dockerfile --push .
+docker buildx build ${EXTRA_OPTS} --platform linux/amd64,linux/arm64/v8 -t ${TAG}:$(get_version) -t ${TAG}:latest -f docker/Dockerfile --push .
